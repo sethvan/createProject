37828a572a06a933af097bb27c3cdc9df2569326
Sethvan <sethvan@yahoo.com>
2022-11-25 16:35:04 -0700

    LICENSE

--- srcCode/replaceFilePhrase/main.cpp
+++ srcCode/replaceFilePhrase/main.cpp
@@ -1,3 +1,31 @@
+/*
+        Copyright 2022 Seth Van Hankins
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/*
+    This cpp file creates the replacePhrase executable.
+    replacePhrase is a command that takes in a filepath, searches the file text
+    for a pattern string and substitutes it with a replacement string and then
+    outputs the mutated version to the output filepath that was specified.
+    Args to command line = inputFilepath, patternString, replacementString, outputFilepath
+
+    It's main intended purpose is to fill in pertinent fields of template
+    files created by the user. Placeholder strings are  positioned inside the
+    templates to be populated by input to the apps created by user.
+*/
+
 #include <stdio.h>
 #include <string.h>
 
